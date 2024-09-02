98a3e9d41e230de6346b7627979ce8bf415c02c1
Toma Krstic <toma.krstic.76@gmail.com>
2024-09-02 09:47:05 +0200

    comment

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,10 @@ import (
 	"github.com/TomasCruz/users/internal/app"
 )
 
+// this approach of app.Start() is taken for two reasons:
+// 1) if e.g. a worker app for listening to Kafka is needed, add another directory and starter file like this one,
+// another directory like internal/app with appropriate content, proper Makefile commands, etc
+// 2) putting everything in internal
 func main() {
 	app.Start()
 }
