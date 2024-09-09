25c8dc9cde1621ef3a6bafa0724ca6c0e455ba9e
Toma Krstic <toma.krstic.76@gmail.com>
2024-09-09 23:55:54 +0200

    test streamlined

--- internal/entities/filter_test.go
+++ internal/entities/filter_test.go
@@ -16,93 +16,71 @@ func TestExtractFilterFromQueryParams(t *testing.T) {
 	tests := []struct {
 		name      string
 		urlString string
-		parseErr  error
-
 		urlValues url.Values
 		m         map[string][]string
 	}{
 		{
 			name:      "no filter",
 			urlString: urlStart,
-			parseErr:  nil,
-
 			urlValues: url.Values{},
 			m:         map[string][]string{},
 		},
 		{
 			name:      "country only",
 			urlString: fmt.Sprintf("%s?country=BIH", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"country": []string{"BIH"}},
 			m:         url.Values{"country": []string{"BIH"}},
 		},
 		{
 			name:      "invalid country",
 			urlString: fmt.Sprintf("%s?country=", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"country": []string{""}},
 			m:         map[string][]string{},
 		},
 		{
 			name:      "country duplicated",
 			urlString: fmt.Sprintf("%s?country=BIH,BIH&country=BIH", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"country": []string{"BIH,BIH", "BIH"}},
 			m:         map[string][]string{"country": {"BIH"}},
 		},
 		{
 			name:      "pagination only",
 			urlString: fmt.Sprintf("%s?page-number=1&page-size=10", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"page-number": []string{"1"}, "page-size": []string{"10"}},
 			m:         map[string][]string{"page-number": {"1"}, "page-size": {"10"}},
 		},
 		{
 			name:      "pagination duplicated",
 			urlString: fmt.Sprintf("%s?page-number=1,1&page-size=10&page-number=1", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"page-number": []string{"1,1", "1"}, "page-size": []string{"10"}},
 			m:         map[string][]string{"page-number": {"1"}, "page-size": {"10"}},
 		},
 		{
 			name:      "pagination bad",
 			urlString: fmt.Sprintf("%s?page-number=&page-size=1", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"page-number": {""}, "page-size": {"1"}},
 			m:         map[string][]string{"page-size": {"1"}},
 		},
 		{
 			name:      "pagination and country",
 			urlString: fmt.Sprintf("%s?page-number=1&page-size=10&country=SRB", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"page-number": {"1"}, "page-size": {"10"}, "country": {"SRB"}},
 			m:         map[string][]string{"page-number": {"1"}, "page-size": {"10"}, "country": {"SRB"}},
 		},
 		{
 			name:      "pagination and country lot of it",
 			urlString: fmt.Sprintf("%s?page-number=1&&country=BIH&page-size=10&page-number=12&country=SRB,HRV", urlStart),
-			parseErr:  nil,
-
 			urlValues: url.Values{"page-number": {"1", "12"}, "page-size": {"10"}, "country": {"BIH", "SRB,HRV"}},
-			m:         map[string][]string{"page-number": {"1", "12"}, "page-size": {"10"}, "country": {"SRB", "HRV", "BIH"}},
+			m:         map[string][]string{"page-number": {"1", "12"}, "page-size": {"10"}, "country": {"BIH", "SRB", "HRV"}},
 		},
 	}
 
 	for _, tt := range tests {
-		u, err := url.Parse(tt.urlString)
-		assert.Equal(t, tt.parseErr, err, tt.name, "url.Parse unexpected error")
+		u, _ := url.Parse(tt.urlString)
+		urlValues := u.Query()
+		assert.Condition(t, func() bool { return equalMaps(tt.urlValues, urlValues) }, tt.name, "equal query param maps expected")
 
-		values := u.Query()
-		assert.Condition(t, func() bool { return equalMaps(tt.urlValues, values) }, tt.name, "equal query param maps expected")
-
-		m := ExtractFilterFromQueryParams(values)
+		m := ExtractFilterFromQueryParams(urlValues)
 		assert.Condition(t, func() bool { return equalMaps(tt.m, m) }, tt.name, "equal filtered query param maps expected")
 	}
 }
